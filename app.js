99e1ef02114a548205b1f2dde31f5f3fec5fe7c0
Cristopher Orellana <cristopherorellana@Chris-MacBook-Pro.local>
2018-08-31 16:45:26 -0300

    con interfaz grafica

--- app.js
+++ app.js
@@ -6,6 +6,23 @@ var fs = require('fs');
 var XLSX = require('xlsx');
 var soap = require('soap');
 var url = "http://smpp2.telecochile.cl:4046/?wsdl";
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'marcelochris0@gmail.com',
+    pass: '78912300aA'
+  }
+});
+
+var mailOptions = {
+  from: 'marcelochris0@gmail.com',
+  to: 'corellanajara@hotmail.com,christianfuenzalidat@gmail.com',
+  subject: 'Error en el servidor de sms',
+  text: 'Probablemente el servidor de sms se cayó por una falla interna'
+};
+
 const app = express()
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
@@ -16,21 +33,22 @@ app.get('/', function (req, res) {
 })
 
 app.post('/', function (req, res) {
-  var form = new formidable.IncomingForm();
-  form.parse(req, function (err, fields, files) {
-    var oldpath = files.archivo.path;
-    console.log(oldpath);
-    var newpath = __dirname + files.archivo.name;
-    console.log(newpath);
-    fs.rename(oldpath, newpath, function (err) {
-      if (err) throw err;
-      var hoja = XLSX.readFile(newpath);
-      var nombres = hoja.SheetNames;
-      var datos = XLSX.utils.sheet_to_json(hoja.Sheets[nombres[0]]);
-      for (var i = 0; i < datos.length; i++) {
-        let numero = datos[i].numeros;
-        let mensaje = datos[i].mensaje;
-        soap.createClient(url, function(err, client) {
+  try {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.archivo.path;
+      console.log(oldpath);
+      var newpath = __dirname + files.archivo.name;
+      console.log(newpath);
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        var hoja = XLSX.readFile(newpath);
+        var nombres = hoja.SheetNames;
+        var datos = XLSX.utils.sheet_to_json(hoja.Sheets[nombres[0]]);
+        for (var i = 0; i < datos.length; i++) {
+          let numero = datos[i].numeros;
+          let mensaje = datos[i].mensaje;
+          soap.createClient(url, function(err, client) {
               var data = { clientid : 'christian' , clientpassword : 'chri2017', ani:'56932841111' ,dnis  :numero , message :mensaje}
               client.submitMsg(data,function(err,resultado){
                 if (err) {
@@ -41,15 +59,24 @@ app.post('/', function (req, res) {
               });
           });
 
+        }
+      });
+    });
+  } catch (e) {
+    // mandar correo de que fallo
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
       }
     });
-  });
-
-
+  }
 
 })
-app.listen(3001, function () {
-  console.log('escuchando puerto 3001!')
+let puerto = 4000;
+app.listen(puerto, function () {
+  console.log('escuchando puerto '+puerto+'!')
 })
 
 /*

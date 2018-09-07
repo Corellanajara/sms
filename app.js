10078331fd09a1deb765089e7004604c7a36fd9c
nombre <you@example.com>
2018-09-07 15:24:08 -0300

    listo basico

--- app.js
+++ app.js
@@ -38,14 +38,10 @@ var mailOptions = {
   text: 'Probablemente el servidor de sms se cayó por una falla interna'
 };
 
-
 const app = express()
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
 
 
 function insertarDatos(datos,id){
@@ -61,7 +57,6 @@ function insertarDatos(datos,id){
     if (err) throw err;
     console.log("id " + result.insertId);
   });
-
 }
 function mandarError(e){
   transporter.sendMail(mailOptions, function(error, info){
@@ -72,64 +67,77 @@ function mandarError(e){
       }
   });
 }
+function error(e){
+  mandarError(e);
+  res.render('home', { error: 'Hubo un error, Intenta mas tarde',nombre:'Cristian',resultado : result,estado:null,contenido:null});
+}
+
 var nombreLista = "";
+// RUTAS
 app.post('/subirLista', function (req, res) {
   let resultado = "salio bien";
-
   try {
-
       nombreLista = req.body.nombre;
-      res.render('subirArchivo', { error: null ,nombre : nombreLista , resultado : "Salio bien" ,contenido :null});
- 
-  
+      res.render('subirArchivo', { error: null ,nombre : nombreLista , resultado : "Salio bien" ,contenido :null});  
   } catch (e) {
-    mandarError(e);
-    console.log(e);
+    error(e);
   }
-
 })
 
-
 app.get('/', function (req, res) {
-  let sql = "select idLista , Nombre from lista where idUsuario = 1";
-  con.query(sql, function (err, result,fields) {
-    if (err) throw err;
-    res.render('home', { error: null,nombre:'Cristian',resultado : result,estado:null,contenido:null});
-  });
+  try{
+      let sql = "select idLista , Nombre from lista where idUsuario = 1";
+      con.query(sql, function (err, result,fields) {
+        if (err) throw err;
+        res.render('home', { error: null,nombre:'Cristian',resultado : result,estado:null,contenido:null});
+      });
+  }catch(e){
+    error(e);
+  }
 })
 
 app.post('/subir', function (req, res) {
-  res.render('subirLista', { error: null,resultado : null,contenido:null});
+  try{ 
+    res.render('subirLista', { error: null,resultado : null,contenido:null});
+  }catch(e){
+    error(e);
+  }  
 })
 
 app.post('/enviarApi', function (req, res) {
   let resultado = "salio bien";
   try {
         let lista = req.body.idLista;
-        let estado = "";
+        let estado = " Ha sido mandado con exito";
         let sql = "select Numero , Mensaje from Contenido where idLista = (?)";
         con.query(sql,lista, function (err, result,fields) {
           if (err) throw err;
           for (var i = result.length - 1; i >= 0; i--) {
             let numero = result[i].Numero;
             let mensaje = result[i].Mensaje;
-
             soap.createClient(url, function(err, client) {
               var data = { clientid : 'christian' , clientpassword : 'chri2017', ani:'56821736345' ,dnis  :numero , message :mensaje}
-              client.submitMsg(data,function(err,r){
-                estado = r;
+              client.submitMsg(data,function(err,r){                                
+                console.log(r);
+                console.log(r[0]);
+                console.log(r.submitMsgResult);
+                console.log(r.submitMsgResult.code);
+                data = [r.submitMsgResult.code,r.submitMsgResult.id,r.submitMsgResult.message]
+                let insercion = "insert into Resultado (Codigo,idSms,respuesta) values (?)";
+                con.query(insercion,[data], function (err, result,fields) {
+                  if (err) throw err;
+                });
                 if (err ) {
                   res.render('home', { error: err , resultado : null ,estado : null,nombre:'Cristian'});
                 }
-
                 });
             });
-
           }
+          console.log(estado);
           res.render('verLista', { error: null,nombre:'Cristian',id:lista,resultado : result,estado:estado,contenido:null});
         }); 
-  } catch (e) {
-    mandarError(e);
+  }catch(e){
+    error(e);
   }
 })
 
@@ -141,10 +149,10 @@ app.post('/verLista', function (req, res) {
         con.query(sql,lista, function (err, result,fields) {
           if (err) throw err;
           console.log(result);
-          res.render('verLista', { error: null,nombre:'Cristian',id:lista,resultado : result,contenido:null});
+          res.render('verLista', { error: null,nombre:'Cristian',estado : null ,id:lista,resultado : result,contenido:null});
         }); 
-  } catch (e) {
-    mandarError(e);
+  }catch(e){
+    error(e);
   }
 })
 
@@ -153,9 +161,7 @@ app.post('/subirArchivo', function (req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
       var oldpath = files.archivo.path;
-      //console.log(oldpath);
       var newpath = __dirname + files.archivo.name;
-      //console.log(newpath);
       var sql = "";
       fs.rename(oldpath, newpath, function (err) {
         if (err) throw err;
@@ -173,73 +179,15 @@ app.post('/subirArchivo', function (req, res) {
           idLista = result.insertId;
           insertarDatos(datos,idLista);
         });
-
         res.render('index', { error: null , nombre : nombreLista, resultado : datos });
-
-
       });
     });
-  } catch (e) {
-    console.log(e);
+  }catch(e){
+    error(e);
   }
-
 })
 
 let puerto = 4000;
 app.listen(puerto, function () {
   console.log('escuchando puerto '+puerto+'!')
 })
-
-  
-  /*
-
-Esto es para subir un excel y direcamente mandarlo mediante el api de teleco
-
-app.post('/', function (req, res) {
-  try {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.archivo.path;
-      console.log(oldpath);
-      var newpath = __dirname + files.archivo.name;
-      console.log(newpath);
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        var hoja = XLSX.readFile(newpath);
-        var nombres = hoja.SheetNames;
-        var resultado = "";
-        var datos = XLSX.utils.sheet_to_json(hoja.Sheets[nombres[0]]);
-        for (var i = 0; i < datos.length; i++) {
-          let numero = datos[i].numeros;
-          let mensaje = datos[i].mensaje;
-          soap.createClient(url, function(err, client) {
-              var data = { clientid : 'christian' , clientpassword : 'chri2017', ani:'56821736345' ,dnis  :numero , message :mensaje}
-              client.submitMsg(data,function(err,r){
-                resultado = r;
-                if (err ) {
-                  res.render('index', { error: err , resultado : null });
-                }
-
-              });
-          });
-        }
-        res.render('index', { error: null , resultado : resultado });
-
-
-      });
-    });
-  } catch (e) {
-    // mandar correo de que fallo
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
-    });
-  }
-
-})
-
-
-*/
